672069e8f8f7ded4537362707378f32cccde1ae6
Jordan Scales <scalesjordan@gmail.com>
2011-07-30 12:35:08 -0400

    Added "add_rest(length)" method. Changed iterate and generate functions to be outside of the class.

--- canon.py
+++ canon.py
@@ -3,15 +3,15 @@ from melopy import *
 m = Melopy('canon', 50)
 melody = []
 
-for start in ['d4', 'a3', 'bm3', 'f#m3', 'g3', 'd3', 'g3', 'a3']:
+for start in ['d4', 'a3', 'b3m', 'f#3m', 'g3', 'd3', 'g3', 'a3']:
 	if start.endswith('m'):
-		scale = m.generate_minor_triad(start[:-1])
+		scale = generate_minor_triad(start[:-1])
 	else:
-		scale = m.generate_major_triad(start)
+		scale = generate_major_triad(start)
 	
 	for note in scale:
 		melody.append(note)
-
+		
 m.add_melody(melody, 0.2)
 m.add_note('e4', 0.2)
 m.render()
