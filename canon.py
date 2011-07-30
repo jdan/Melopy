746279490412493988c6a9b3df35ec50557c528a
Jordan Scales <scalesjordan@gmail.com>
2011-07-30 12:45:13 -0400

    fixed bug with major and minor triads

--- canon.py
+++ canon.py
@@ -5,9 +5,9 @@ melody = []
 
 for start in ['d4', 'a3', 'b3m', 'f#3m', 'g3', 'd3', 'g3', 'a3']:
 	if start.endswith('m'):
-		scale = m.generate_minor_triad(start[:-1])
+		scale = generate_minor_triad(start[:-1])
 	else:
-		scale = m.generate_major_triad(start)
+		scale = generate_major_triad(start)
 	
 	for note in scale:
 		melody.append(note)
