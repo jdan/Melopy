8e09bec5808a33de5a2644e818b5ab9e47f876b0
Jordan Scales <scalesjordan@gmail.com>
2011-07-30 20:01:52 -0400

    changed variable to accomodate new scale generation

--- canon.py
+++ canon.py
@@ -5,13 +5,17 @@ melody = []
 
 for start in ['d4', 'a3', 'b3m', 'f#3m', 'g3', 'd3', 'g3', 'a3']:
 	if start.endswith('m'):
-		scale = generate_minor_triad(start[:-1])
+		scale = generate_minor_scale(start[:-1])
 	else:
-		scale = generate_major_triad(start)
+		scale = generate_major_scale(start)
+		
+	scale.insert(0, scale[0][:-1] + str(int(scale[0][-1]) - 1))
+		
+	[melody.append(note) for note in scale]
 	
-	for note in scale:
-		melody.append(note)
-
 m.add_melody(melody, 0.2)
-m.add_note('e4', 0.2)
+m.add_rest(0.4)
+m.add_note('d4', 0.4)
+m.add_rest(0.1)
+m.add_note('d5', 0.8)
 m.render()
