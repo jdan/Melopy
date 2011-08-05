01ed0764e3483dcf68ef12eb45b734fb8304cb40
Jordan Scales <scalesjordan@gmail.com>
2011-08-05 13:55:06 -0400

    changed to triangle wave to show various wave types

--- twinkle.py
+++ twinkle.py
@@ -1,7 +1,7 @@
 from melopy import *
 
 song = Melopy('twinkle')
-t = 0.25
+t = 0.35
 
 part1notes = ['C', 'G', 'A', 'G', 'F', 'E', 'D', 'C']
 part2notes = ['G', 'F', 'E', 'D']
@@ -9,10 +9,11 @@ part2notes = ['G', 'F', 'E', 'D']
 def twinkle(notes):
 	for i in range(len(notes)):
 		if i % 4 == 3:
-			song.add_note(notes[i], t*2)
+			song.add_note(notes[i], t, 'triangle')
+			song.add_rest(t)
 		else:
-			song.add_note(notes[i], t)
-			song.add_note(notes[i], t)
+			song.add_note(notes[i], t, 'triangle')
+			song.add_note(notes[i], t, 'triangle')
 			
 twinkle(part1notes)
 twinkle(part2notes)
