d2499848d4f920005d3534f2732b00a1b95ef505
Jordan Scales <scalesjordan@gmail.com>
2011-09-04 18:05:09 -0400

    more includes

--- melopy/__init__.py
+++ melopy/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from melopy import *
+from melopy  import *
+from scales  import *
+from utility import *
 
 # Licensed under The MIT License (MIT)
 # See LICENSE file for more
