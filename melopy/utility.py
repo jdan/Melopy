ad0e17d44c384c952a35a6b5acc72d1def498175
Alex Freidah <alexfreidah@Alexs-MacBook-Pro.local>
2011-11-06 20:08:47 -0500

    added some comments as i was reading through just for personal benefit

--- melopy/utility.py
+++ melopy/utility.py
@@ -10,14 +10,20 @@ def key_from_note(note):
     """Returns the key number (keys from A0) from a note represented by a string"""
     indices = { 'C':0, 'D':2, 'E':4, 'F':5, 'G':7, 'A':9, 'B':11 }
 
+    """Set the octave to 4 by default incase it has not been specified"""
     octave = 4
 
+    """But lets check if the octave WAS specified and change it if it was"""
     if note[-1] in '012345678':
         octave = int(note[-1])
 
+    """"get the tone of the base note of the scale"""
     tone = indices[note[0].upper()]
+    
+    """get the exact pitch of the starting tone at the correct octave"""
     key = 12 * octave + tone
 
+    """if a sharp sign was given then move the key up by a half step, if flat then down a half step"""
     if len(note) > 1 and note[1] == '#':
         key += 1
     elif len(note) > 1 and note[1] == 'b':
@@ -28,10 +34,12 @@ def key_from_note(note):
 def note_from_key(key):
     """Returns a string representing a note which is (key) keys from A0"""
     notes = ['a','a#','b','c','c#','d','d#','e','f','f#','g','g#']
+
     octave = (key + 8) / 12
     note = notes[(key -1 ) % 12]
 
     return note.upper() + str(octave)
+
 
 # Licensed under The MIT License (MIT)
 # See LICENSE file for more
