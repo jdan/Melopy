5fedbc81ad5fc5012d10f31a2827e15bc4bf12b9
Jordan Scales <scalesjordan@gmail.com>
2012-01-04 17:43:37 -0500

    added left-hand notes for furelise.py, included meeps. also fixed timing of rests in the right hand portion

--- examples/furelise.py
+++ examples/furelise.py
@@ -4,6 +4,7 @@ import os
 def main():
     m = Melopy('furelise')
     m.parsefile(os.path.dirname(__file__) + '/meeps/furelise.mp')
+    m.parsefile(os.path.dirname(__file__) + '/meeps/furelise_left.mp', 0)
     m.render()
     
 if __name__ == '__main__':
