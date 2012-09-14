c0fe3fa1204d267c473aa556890b50b5fa619168
Alex Freidah <alexfreidah@Alexs-iMac.local>
2012-09-14 16:57:10 -0400

    updated to the latest utility.py and melopy.py from prezjordan's repository as mine had gone out of date

--- melopy/melopy.py
+++ melopy/melopy.py
@@ -7,20 +7,32 @@ import os, sys
 from utility import *
 from scales  import *
 
+# same included wave functions
+# a function of frequency and tick
+#   each function accepts the frequency and tick,
+#   and returns a value from -1 to 1
+
+sine     = lambda f, t: math.sin(2 * math.pi * t * f / 44100.0)
+square   = lambda f, t: 0.6 * ((t % (44100 / f) >= ((44100 / f)/2)) * 2 - 1)
+sawtooth = lambda f, t: (t % (44100 / f)) / (44100 / f) * 2 - 1
+def triangle(f, t):
+    v = 2 * (t % (44100 / f)) / (44100 / f)
+    if t % (44100 / f) >= (44100 / (2 * f)):
+        v = 2 * 1 - v
+    v = 2 * v - 1
+    return v
+
 class Melopy:
-    def __init__(self, title='sound', volume=50, tempo=120, octave=4):
+    def __init__(self, title='sound', volume=20, tempo=120, octave=4):
         self.title = title.lower()
         self.rate = 44100
         self.volume = volume
         self.data = []
-        
+
         self.tempo = tempo
         self.octave = octave
-        self.wave_type = 'sine'
-        
-    def isithere(self):
-        print 'lol'
-        
+        self.wave_type = sine
+
     def add_wave(self, frequency, length, location='END'):
         if location == 'END':
             location = len(self.data)
@@ -28,28 +40,14 @@ class Melopy:
             location = 0
         elif location * 44100 > len(self.data):
             location = len(self.data) / 44100.0
-        
+
         # location is a time, so let's adjust
         location = int(location * 44100)
-        
+
         for n in range(0, int(44100 * length)):
-            period = 44100.0 / frequency
-            
-            if self.wave_type == 'square':
-                val = ((n % int(period) >= (int(period)/2)) * 2) - 1
-                val *= 0.6
-            elif self.wave_type == 'sawtooth':
-                val = ((n % int(period)) / period * 2) - 1
-            elif self.wave_type == 'triangle':
-                val = 2 * (n % int(period)) / period
-                if n % int(period) >= (int(period) / 2):
-                    val = 2 * 1 - val
-                val = 2 * val - 1
-            else: # default to sine
-                val = math.sin(2 * math.pi * n / period)
-                
+            val = self.wave_type(frequency, n)
             val *= self.volume / 100.0 * 32767
-            
+
             if location + n >= len(self.data):
                 self.data.append(val)
             else:
@@ -60,9 +58,9 @@ class Melopy:
                     val = -32768
                 else:
                     val += current_val
-                    
+
                 self.data[location + n] = val
-                    
+
     def add_note(self, note, length, location='END'):
         """Add a note, or if a list, add a chord."""
         if not isinstance(note, list):
@@ -75,66 +73,118 @@ class Melopy:
             if item[-1] not in '0123456789':
                 item += str(self.octave)
 
-            self.add_wave(frequency_from_note(item), length, location)
-        
+            self.add_wave(note_to_frequency(item, self.octave), length, location)
+
     def add_melody(self, melody, length):
         for note in melody:
             if note[-1] not in '0123456789':
                 note += self.octave
-            self.add_wave(frequency_from_note(note), length)
-            
-    def add_whole_note(self, note):
+            self.add_wave(note_to_frequency(note), length)
+
+    def add_whole_note(self, note, location='END'):
         """Add a whole note"""
-        self.add_note(note, 60.0 / self.tempo * 4)
-        
-    def add_half_note(self, note):
+        self.add_fractional_note(note, 1.0, location)
+
+    def add_half_note(self, note, location='END'):
         """Add a half note"""
-        self.add_note(note, 60.0 / self.tempo * 2)
-        
-    def add_quarter_note(self, note):
+        self.add_fractional_note(note, 1.0 / 2, location)
+
+    def add_quarter_note(self, note, location='END'):
         """Add a quarter note"""
-        self.add_note(note, 60.0 / self.tempo)
-        
-    def add_eighth_note(self, note):
+        self.add_fractional_note(note, 1.0 / 4, location)
+
+    def add_eighth_note(self, note, location='END'):
         """Add a eigth note"""
-        self.add_note(note, 60.0 / self.tempo / 2)
-        
-    def add_sixteenth_note(self, note):
+        self.add_fractional_note(note, 1.0 / 8, location)
+
+    def add_sixteenth_note(self, note, location='END'):
         """Add a sixteenth note"""
-        self.add_note(note, 60.0 / self.tempo / 4)
-        
-    def add_fractional_note(self, note, fraction):
+        self.add_fractional_note(note, 1.0 / 16, location)
+
+    def add_fractional_note(self, note, fraction, location='END'):
         """Add a fractional note (smaller then 1/16 notes)"""
-        self.add_note(note, 60.0 / self.tempo * (fraction * 4))
-        
+        self.add_note(note, 60.0 / self.tempo * (fraction * 4), location)
+
     def add_rest(self, length):
         for i in range(int(self.rate * length)):
             self.data.append(0)
-            
+
     def add_whole_rest(self):
-        self.add_rest(60.0 / self.tempp * 4)
-        
+        self.add_fractional_rest(1.0)
+
     def add_half_rest(self):
-        self.add_rest(60.0 / self.tempo * 2)
-            
+        self.add_fractional_rest(1.0 / 2)
+
     def add_quarter_rest(self):
-        self.add_rest(60.0 / self.tempo)
-        
+        self.add_fractional_rest(1.0 / 4)
+
     def add_eighth_rest(self):
-        self.add_rest(60.0 / self.tempo / 2)
-        
+        self.add_fractional_rest(1.0 / 8)
+
     def add_sixteenth_rest(self):
-        self.add_rest(60.0 / self.tempo / 4)
-        
+        self.add_fractional_rest(1.0 / 16)
+
     def add_fractional_rest(self, fraction):
         self.add_rest(60.0 / self.tempo * (fraction * 4))
-        
+
+    def parse(self, string, location='END'):
+        tracks = string.split('&&&')
+
+        # special case for multiple tracks
+        if len(tracks) > 1:
+            t = len(self.data) / 44100.0
+            for track in tracks:
+                self.parse(track, t)
+            return
+
+        cf = 0.25                    # start with a quarter note, change accordingly
+        in_comment = False
+
+        for i, char in enumerate(string):        # divide melody into fragments
+            # / this is a comment /
+            if char == '/':
+                in_comment = not in_comment
+
+            if in_comment:
+                continue
+            elif char in 'ABCDEFG':
+                if (i+1 < len(string)) and (string[i+1] in '#b'):
+                    # check if the next item in the array is
+                    #    a sharp or flat, make sure we include it
+                    char += string[i+1]
+
+                self.add_fractional_note(char, cf, location)
+                if location != 'END':
+                    location += (60.0 / self.tempo * (cf * 4))
+            elif char in map(str, range(0, 20)):
+                self.octave = int(char)
+            elif char == '+' or char == '^':
+                self.octave += 1
+            elif char == 'V' or char == 'v' or char == '-':
+                self.octave -= 1
+            elif char == '(' or char == ']':
+                cf /= 2
+            elif char == ')' or char == '[':
+                cf *= 2
+            elif char == '_':
+                self.add_fractional_rest(cf)
+                if location != 'END':
+                    location += (60.0 / self.tempo * (cf * 4))
+
+    def parsefile(self, filename, location='END'):
+        fr = open(filename, 'r')
+        s = fr.read()
+        fr.close()
+
+        self.parse(s, location)
+
     def render(self):
         """Render a playable song out to a .wav file"""
         melopy_writer = wave.open(self.title + '.wav', 'w')
         melopy_writer.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
         p = -1
-        
+        data_frames = []
+
         for i in range(len(self.data)):
             q = 100 * i / len(self.data)
             if p != q:
@@ -142,13 +192,15 @@ class Melopy:
                 sys.stdout.flush()
                 p = q
             packed_val = struct.pack('h', int(self.data[i]))
-            melopy_writer.writeframes(packed_val)
-            melopy_writer.writeframes(packed_val)
-            
+            data_frames.append(packed_val)
+            data_frames.append(packed_val)
+
+        melopy_writer.writeframes(''.join(data_frames))
+
         sys.stdout.write("\r[%s] 100%%" % ('='*50))
         sys.stdout.flush()
         sys.stdout.write("\nDone\n")
         melopy_writer.close()
-        
+
 # Licensed under The MIT License (MIT)
 # See LICENSE file for more
--- melopy/utility.py
+++ melopy/utility.py
@@ -1,29 +1,39 @@
-def frequency_from_key(key):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from math import log
+
+def key_to_frequency(key):
     """Returns the frequency of the note (key) keys from A0"""
     return 440 * 2 ** ((key - 49) / 12.0)
 
-def frequency_from_note(note):
-    """Returns the frequency of a note represented by a string"""
-    return frequency_from_key(key_from_note(note))
+def key_to_note(key, octaves=True):
+    """Returns a string representing a note which is (key) keys from A0"""
+    notes = ['a','a#','b','c','c#','d','d#','e','f','f#','g','g#']
+    octave = (key + 8) / 12
+    note = notes[(key - 1) % 12]
 
-def key_from_note(note):
+    if octaves:
+        return note.upper() + str(octave)
+    else:
+        return note.upper()
+
+def note_to_frequency(note, default=4):
+    """Returns the frequency of a note represented by a string"""
+    return key_to_frequency(note_to_key(note, default))
+
+def note_to_key(note, default=4):
     """Returns the key number (keys from A0) from a note represented by a string"""
     indices = { 'C':0, 'D':2, 'E':4, 'F':5, 'G':7, 'A':9, 'B':11 }
 
-    """Set the octave to 4 by default incase it has not been specified"""
-    octave = 4
+    octave = default
 
-    """But lets check if the octave WAS specified and change it if it was"""
     if note[-1] in '012345678':
         octave = int(note[-1])
 
-    """"get the tone of the base note of the scale"""
     tone = indices[note[0].upper()]
-    
-    """get the exact pitch of the starting tone at the correct octave"""
     key = 12 * octave + tone
 
-    """if a sharp sign was given then move the key up by a half step, if flat then down a half step"""
     if len(note) > 1 and note[1] == '#':
         key += 1
     elif len(note) > 1 and note[1] == 'b':
@@ -31,15 +41,11 @@ def key_from_note(note):
 
     return key - 8;
 
-def note_from_key(key):
-    """Returns a string representing a note which is (key) keys from A0"""
-    notes = ['a','a#','b','c','c#','d','d#','e','f','f#','g','g#']
+def frequency_to_key(frequency):
+    return int(12 * log(frequency/440.0) / log(2) + 49)
 
-    octave = (key + 8) / 12
-    note = notes[(key -1 ) % 12]
-
-    return note.upper() + str(octave)
-
+def frequency_to_note(frequency):
+    return key_to_note(frequency_to_key(frequency))
 
 # Licensed under The MIT License (MIT)
 # See LICENSE file for more
