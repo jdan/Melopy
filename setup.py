2f74853ed28766018bef9aa57df8919083aaa854
Jordan Scales <scalesjordan@gmail.com>
2012-09-17 15:05:49 -0400

    removed setuptools dependency, removed unicode formatting for pip

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from distutils.core import setup
 
 config = {
-        'name': u'Melopy',
-        'author': u'Jordan Scales',
-        'author_email': u'none',
-        'description': u'Python music library',
-        'long_description': open(u'README.txt').read(),
-        'packages': ['melopy'],
-        'version': u'0.1.0',
-        'url': u'https://github.com/prezjordan/Melopy',
-        'license': 'LICENSE.txt',
-        'classifiers': [],
+    'name': 'Melopy',
+    'author': 'Jordan Scales',
+    'author_email': 'scalesjordan@gmail.com',
+    'description': 'Python music library',
+    'long_description': open('README.txt').read(),
+    'packages': ['melopy'],
+    'version': '0.1.0',
+    'url': 'https://github.com/prezjordan/Melopy',
+    'license': 'LICENSE.txt',
+    'classifiers': []
 }
 
 setup(**config)
 
 # Licensed under The MIT License (MIT)
 # See LICENSE file for more
-
