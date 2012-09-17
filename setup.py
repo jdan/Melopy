55af790c41400c697eaecffc6aefa32880703cb9
parent5446 <tylerromeo@gmail.com>
2012-09-17 12:26:49 -0400

    Added necessary files for PIP packaging.
    Added MANIFEST.in and other necessary files
    for packaging. Also removed usage of find_packages(),
    since it does not exist in distutils.
    
    Signed-off-by: parent5446 <tylerromeo@gmail.com>

--- setup.py
+++ setup.py
@@ -2,22 +2,23 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
-    from distutils.core import setup, find_packages
+    from distutils.core import setup
 
 config = {
         'name': u'Melopy',
         'author': u'Jordan Scales',
-        'author_email': u'',
+        'author_email': u'none',
         'description': u'Python music library',
-        'long_description': open(u'README.markdown').read(),
-        'packages': find_packages(),
+        'long_description': open(u'README.txt').read(),
+        'packages': ['melopy'],
         'install_requires': [],
         'tests_require': ['nose'],
         'test_suite': u'nose.collector',
-        'version': u'',
+        'version': u'0.1.0',
         'url': u'https://github.com/prezjordan/Melopy',
+        'license': 'LICENSE.txt',
         'classifiers': [],
 }
 
